731f5fc9e51a10a1dc46dc8734138c7182d85517
Imanina Majeed <imanina@recovision.net>
2024-11-07 10:56:11 +0800

    Remove unused Geist font imports from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,6 @@ import '@/app/styles/globals.css';
 import Navbar from "@/app/ui/navbar";
 import { inter } from "@/app/fonts/fonts";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: {
     template: "%s | Imanina's Portfolio",
